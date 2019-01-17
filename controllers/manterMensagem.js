5bc62ccb8f066f34f88ba43b9217577b6e43871d
eng-edu <eduardo.eng15@hotmail.com>
2019-01-17 19:39:38 -0200

    UP

--- controllers/manterMensagem.js
+++ controllers/manterMensagem.js
@@ -5,7 +5,7 @@ const execute = require('../executeSQL');
 socket.on('connection', (io) => {
     io.on('LIST_MSG', (id) => {
         var s = 'msg' + id;
-        var sqlQry = `SELECT * FROM MENSAGEM WHERE ARCO_ID = ${id}`;
+        var sqlQry = `SELECT * FROM MENSAGEM WHERE ID_ARCO = ${id}`;
         execute.executeSQL(sqlQry, function (results) {
             if (results.length > 0) {
                 io.emit(s, results);
@@ -15,15 +15,17 @@ socket.on('connection', (io) => {
     });
 });
 
-exports.post = ('/sendMSG/:TEXTO/:ID_AUTOR/:EMAIL_AUTOR/:DATA/:ARCO_ID', (req, res) => {
+exports.post = ('/inserir/:ID_USUARIO/:TEXTO/:DATA/:HORA/:ID_ARCO', (req, res) => {
 
-    const TEXTO = req.params.TEXTO;
-    const ID_AUTOR = req.params.ID_AUTOR;
+  
+    const ID_USUARIO = req.params.ID_USUARIO;
+    const TEXTO = req.params.TEXTO
     const DATA = req.params.DATA;
-    const ARCO_ID = req.params.ARCO_ID;
-    var EMAIL_AUTOR = req.params.EMAIL_AUTOR
+    const HORA = req.params.HORA;
+    const ARCO_ID = req.params.ID_ARCO
 
-    var sqlQry = `INSERT INTO MENSAGEM (TEXTO, ID_AUTOR, EMAIL_AUTOR, DATA, ARCO_ID) VALUES ('${TEXTO}',${ID_AUTOR},'${EMAIL_AUTOR}','${DATA}',${ARCO_ID})`;
+    var sqlQry = `INSERT INTO MENSAGEM (ID_USUARIO, TEXTO, DATA, HORA, ARCO_ID) 
+            VALUES (${ID_USUARIO},'${TEXTO}','${DATA}','${HORA}','${ARCO_ID}')`;
 
     execute.executeSQL(sqlQry, function (results) {
 
