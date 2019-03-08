fcd84de1493912898f3d935e934a9fc30c429390
eng-edu <eduardo.eng15@hotmail.com>
2019-03-08 17:32:51 -0300

    up

--- server/serverSocket.js
+++ server/serverSocket.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const io = require('socket.io')();
-io.on('connection',(socket)=>{
+const execute = require('../executeSQL');
+
+
+io.on('connection', (socket) => {
     console.log('conectou: ' + socket.id)
-    socket.on('disconnect', function(){
+
+    console.log('id_usuario: ' + socket.handshake.query.ID_USUARIO)
+
+    var sqlQry = `UPDATE USUARIO SET ONLINE = 1 WHERE ID = ${socket.handshake.query.ID_USUARIO}`;
+    execute.executeSQL(sqlQry, function (results) {
+ 
+       socket.emit('ON'+socket.handshake.query.ID_USUARIO, '1');
+       socket.broadcast.emit('ON'+socket.handshake.query.ID_USUARIO, '1');
+
+    });
+
+    socket.on('disconnect', function () {
         console.log('desconectou: ' + socket.id)
+        console.log('id_usuario: ' + socket.handshake.query.ID_USUARIO)
+        var sqlQry = `UPDATE USUARIO SET ONLINE = 0 WHERE ID = ${socket.handshake.query.ID_USUARIO}`;
+        execute.executeSQL(sqlQry, function (results) {
+            socket.emit('ON'+socket.handshake.query.ID_USUARIO, '0');
+            socket.broadcast.emit('ON'+socket.handshake.query.ID_USUARIO, '0');
+        });
     });
 });
 
