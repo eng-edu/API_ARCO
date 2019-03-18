7f18903a67eaf8d357d47bd3b670e20a4ecaedd8
eng-edu <eduardo.eng15@hotmail.com>
2019-03-17 21:11:48 -0300

    up

--- controllers/manterOpiniao.js
+++ controllers/manterOpiniao.js
@@ -36,7 +36,7 @@ exports.novaOpiniao = ('/novaOpiniao/:ID_LIDER/:ID_TEMATICA', (req, res) => {
 
     //SITUACAO 1 = EM DESENVOLVIMENTO
     //SITUACAO 2 = FINALIZADO
- 
+
     var sqlQry = `INSERT INTO OPINIAO(ID_ETAPA, ID_USUARIO, SITUACAO, DATA_HORA) VALUES (${ID_USUARIO}, ${ID_ETAPA}, '1', '${DATA_HORA}')`;
 
     execute.executeSQL(sqlQry, function (results) {
@@ -48,3 +48,70 @@ exports.novaOpiniao = ('/novaOpiniao/:ID_LIDER/:ID_TEMATICA', (req, res) => {
 
     });
 });
+
+exports.buscarOpiniao2 = ('/buscarOpiniao/:ID_USUARIO/:ID_ETAPA', (req, res) => {
+
+    const ID_USUARIO = req.params.ID_USUARIO;
+    const ID_ETAPA = req.params.ID_ETAPA;
+
+    var sqlQry = `SELECT  o.TEXTO
+FROM
+    OPINIAO AS o
+        INNER JOIN
+    ETAPA AS e ON o.ID_ETAPA = e.ID
+WHERE
+    ID_ETAPA = ${ID_ETAPA} AND o.SITUACAO = 1 AND o.ID_USUARIO = ${ID_USUARIO};`;
+
+    console.log(sqlQry)
+
+    execute.executeSQL(sqlQry, function (results) {
+        if (results.length > 0) {
+            console.log(results)
+            res.status(200).send(results[0]);
+        } else {
+
+            inserirOpiniao(ID_USUARIO, ID_ETAPA, res)
+        }
+
+    });
+});
+
+
+function inserirOpiniao(ID_USUARIO, ID_ETAPA, res) {
+
+    var DATA_HORA = require('./util').dataAtual();
+
+    //SITUACAO 1 = EM DESENVOLVIMENTO
+    //SITUACAO 2 = FINALIZADO
+
+    var sqlQry = `INSERT INTO OPINIAO(ID_ETAPA, ID_USUARIO, SITUACAO, DATA_HORA, TEXTO) VALUES (${ID_ETAPA}, ${ID_USUARIO}, '1', '${DATA_HORA}', 'Desenvovlda seu tetxo aqui!')`;
+
+    execute.executeSQL(sqlQry, function (results) {
+        if (results['insertId'] > 0) {
+            buscarOpiniao3(ID_USUARIO, ID_ETAPA, res)
+        } else {
+            res.status(203).send(results);
+        }
+
+    })
+}
+
+
+function buscarOpiniao3(ID_USUARIO, ID_ETAPA, res) {
+    var sqlQry = `SELECT  o.TEXTO
+FROM
+    OPINIAO AS o
+        INNER JOIN
+    ETAPA AS e ON o.ID_ETAPA = e.ID
+WHERE
+    ID_ETAPA = ${ID_ETAPA} AND o.SITUACAO = 1 AND o.ID_USUARIO = ${ID_USUARIO};`;
+
+    execute.executeSQL(sqlQry, function (results) {
+        if (results.length > 0) {
+            res.status(200).send(results[0]);
+        } else {
+            res.status(203).send(results)
+        }
+
+    });
+}
--- routes/routerOpiniao.js
+++ routes/routerOpiniao.js
@@ -9,6 +9,9 @@ const manterOpiniao = require('../controllers/manterOpiniao')
 
 //rotas
 router.post('/novaOpiniao/:ID_LIDER/:ID_TEMATICA', manterOpiniao.novaOpiniao)
+//rotas
+router.get('/buscarOpiniao/:ID_USUARIO/:ID_ETAPA', manterOpiniao.buscarOpiniao2)
+
 
 
 //exporta o modulo
