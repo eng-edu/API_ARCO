7d284046d3d04d23f8e53ac13ba15ddd0ac0c8d6
eng-edu <eduardo.eng15@hotmail.com>
2019-03-29 23:34:30 -0300

    up

--- controllers/manterComentario.js
+++ controllers/manterComentario.js
@@ -4,14 +4,13 @@ const execute = require('../executeSQL');
 
 
 socket.on('connection', (io) => {
-    
-    io.on('COMENTARIO', function (ID_OPINIAO) {    
-       buscarComentarios( io,ID_OPINIAO)
+
+    io.on('COMENTARIO', function (ID_OPINIAO) {
+        buscarComentarios(io, ID_OPINIAO)
     })
 
-    io.on('MEU_COMENTARIO', function (results) { 
-
-
+    io.on('MEU_COMENTARIO', function (results) {
+        comentar(io, results.ID_USUARIO, results.ID_OPINIAO, results.TEXTO)
     })
 
 });
@@ -19,7 +18,30 @@ socket.on('connection', (io) => {
 
 function buscarComentarios(io, ID_OPINIAO) {
     var msg = 'COMENTARIO' + ID_OPINIAO;
-    var sqlQry = `SELECT c.ID_USUARIO AS ID_AUTOR, u.EMAIL AS EMAIL_AUTOR, c.TEXTO, c.DATA_HORA FROM COMENTARIO AS c INNER JOIN USUARIO AS u ON c.ID_USUARIO = u.ID WHERE ID_OPINIAO = '${ID_OPINIAO}'`;
+    var sqlQry = `SELECT 
+    c.ID_USUARIO AS ID_AUTOR, c.TEXTO, c.DATA_HORA, a.ID_LIDER
+FROM
+    COMENTARIO AS c
+        INNER JOIN
+    USUARIO AS u ON c.ID_USUARIO = u.ID
+        INNER JOIN
+    OPINIAO AS o ON o.ID = c.ID_OPINIAO
+        INNER JOIN
+    ETAPA AS e ON o.ID_ETAPA = e.ID
+        INNER JOIN
+    ARCO AS a ON e.ID_ARCO = a.ID
+WHERE c.ID_OPINIAO = ${ID_OPINIAO}`;
+    execute.executeSQL(sqlQry, function (results) {
+        io.emit(msg, results);
+        io.broadcast.emit(msg, results);
+    });
+}
+
+
+function comentar(io, ID_USUARIO, ID_OPINIAO, TEXTO) {
+    var DATA_HORA = require('./util').dataAtual();
+    var msg = 'COMENTARIO' + ID_OPINIAO;
+    var sqlQry = `INSERT INTO COMENTARIO (ID_USUARIO, ID_OPINIAO, TEXTO, DATA_HORA) VALUES (${ID_USUARIO}, ${ID_OPINIAO}, '${TEXTO}', '${DATA_HORA}')`;
     execute.executeSQL(sqlQry, function (results) {
         io.emit(msg, results);
         io.broadcast.emit(msg, results);
